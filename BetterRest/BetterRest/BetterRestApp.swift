7b68bca62baf64d21140ab43940241630708f152
Quinn Liu <quinn2liu@gmail.com>
2024-07-25 00:03:10 -0400

    BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Quinn Liu on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
